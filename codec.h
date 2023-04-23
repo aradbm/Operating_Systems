787d39b6aece559e3405809af4fbfb6783c4c294
nivmoti <93876502+nivmoti@users.noreply.github.com>
2023-04-23 20:24:27 +0300

    Create codec.h

---
+++ codec.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#define N 1024
+
+int encode(char * src, char * dst, int len);
+int decode (char * src, char * dst, int len);
+// int decodetwo(char * src, char * dst, int len);
+// int encodetwo(char * src, char * dst, int len);
