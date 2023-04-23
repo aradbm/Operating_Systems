5cffb4f442dff8e7ba6ad4249866a4756b11b5df
nivmoti <93876502+nivmoti@users.noreply.github.com>
2023-04-23 20:22:37 +0300

    Update codecB.c

--- codecB.c
+++ codecB.c
@@ -1,17 +1,52 @@
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <ctype.h>
-#include <stdbool.h>
-char *encode(char *string)
-{
-    char *changed = "";
-    strcpy(changed, string);
-    return changed;
+#ifndef TEST_H_INCLUDED
+#define TEST_H_INCLUDED
+
+#include "codec.h"
+
+#endif
+
+int encode(char * src, char * dst, int len){
+    int i=0;
+    while( i<len){
+        if (isalpha(src[i])){
+            if (isalpha(src[i]+3)){
+                int newChar=src[i]+3;
+                char ch=newChar;
+                src[i]=ch;
+            }
+            else{
+                int newChar=src[i]-123+3+65;
+                char ch=newChar;
+                src[i]=ch;
+            }  
+        }
+        i+=1;
+    }
+    return 0;
 }
-char *decode(char *string)
-{
-    char *changed = "";
-    strcpy(changed, string);
-    return changed;
+
+int decode(char * src, char * dst, int len){
+    int i=0;
+    while( i<len){
+       if (isalpha(src[i])){
+            if (isalpha(src[i]-3)){
+                int newChar=src[i]-3;
+                char ch=newChar;
+                src[i]=ch;
+            }
+            else{
+                int newChar=src[i]-65-3+122;
+                char ch=newChar;
+                src[i]=ch;
+            }  
+        }
+        i+=1;
+    }
+    return 0;
+}
+
+
+int main(){
+    printf("wrong name for the shared file\n");
+    return 0;
 }
