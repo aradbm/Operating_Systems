bde5ef35ec25e049aa63d5e54db4ebaf14d0930f
aradbm <aradbm@gmail.com>
2023-05-08 09:35:19 +0300

    commit server

--- Task_3/client.c
+++ Task_3/client.c
@@ -5,11 +5,17 @@
 #include <arpa/inet.h>
 #include <stdlib.h>
 
-#define PORT 9990
+#define PORT 8081
 #define SIZE 1024
 
 int main(int argc, char *argv[])
 {
+    if (argc < 3)
+    {
+        printf("Usage: %s <host> <port>\n", argv[0]);
+        exit(1);
+    }
+
     char *hostArgv = argv[1];
     char *portArgv = argv[2];
 
@@ -17,19 +23,31 @@ int main(int argc, char *argv[])
 
     int client_socket = socket(AF_INET, SOCK_STREAM, 0);
 
+    if (client_socket == -1)
+    {
+        perror("socket");
+        exit(1);
+    }
+
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_port = htons(port);
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    inet_aton(hostArgv, &(addr.sin_addr));
 
-    int server_socket = connect(client_socket, (struct sockaddr *)&addr, sizeof(addr)); //连接服务器
-
-    printf("Connect result ==> %d\n", server_socket);
-    if (server_socket < 0) {
-        perror("Connect result");
+    if (inet_aton(hostArgv, &(addr.sin_addr)) == 0)
+    {
+        perror("inet_aton");
         exit(1);
     }
+
+    int server_socket = connect(client_socket, (struct sockaddr *)&addr, sizeof(addr)); // 连接服务器
+
+    if (server_socket == -1)
+    {
+        perror("connect");
+        exit(1);
+    }
+
+    printf("Connect result ==> %d\n", server_socket);
 
     char buf[SIZE] = {0};
 
@@ -37,7 +55,13 @@ int main(int argc, char *argv[])
     {
         printf("Send to server：");
         scanf("%s", buf);
-        write(client_socket, buf, strlen(buf));
+        ssize_t num_bytes_sent = write(client_socket, buf, strlen(buf));
+
+        if (num_bytes_sent == -1)
+        {
+            perror("write");
+        }
+
         printf("\n");
         if (strncmp(buf, "end", 3) == 0)
         {
--- Task_3/server.c
+++ Task_3/server.c
@@ -8,35 +8,39 @@
 #include <string.h>
 #include <sys/time.h>
 #include <poll.h>
-
-#define PORT 9990
 #define SIZE 1024
 #define MAX_CLIENTS 10
-
-int creat_socket()
+int creat_socket(int port, const char *server_ip)
 {
     int server_socket = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in addr;
 
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(PORT);
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(port);
+
+    if (inet_aton(server_ip, &addr.sin_addr) == 0)
+    {
+        perror("inet_aton");
+        exit(1);
+    }
 
     int bindResult = bind(server_socket, (struct sockaddr *)&addr, sizeof(addr));
     if (bindResult == -1)
     {
         perror("bindResult");
+        exit(1);
     }
 
     int listenResult = listen(server_socket, 5);
     if (listenResult == -1)
     {
         perror("listenResult");
+        exit(1);
     }
+
     printf("server start\n");
     return server_socket;
 }
-
 int wait_client(int server_socket)
 {
     struct pollfd pollfds[MAX_CLIENTS + 1];
@@ -100,16 +104,18 @@ int wait_client(int server_socket)
     }
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-    int server_socket = creat_socket();
-
+    if (argc < 3)
+    {
+        printf("Usage: %s <server_ip> <port>\n", argv[0]);
+        exit(1);
+    }
+    const char *server_ip = argv[1];
+    int port = atoi(argv[2]);
+    int server_socket = creat_socket(port, server_ip);
     int client_socket = wait_client(server_socket);
-
-    printf("server end\n");
-
     close(client_socket);
     close(server_socket);
-
     return 0;
 }
